a7368e93883099ec932342780aeeb4bd6744a951
darms <darmcca@gmail.com>
2020-05-12 16:45:13 -0700

    refactoring components

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
 import './App.css';
 
 class App extends Component {
-  render() {
+  componentDidMount() {
+    console.log(1234);
 
+  }
+
+
+  render() {
+   
   
     return (
       <div className='App'>
       <Navbar /> 
+      <div className= "container">
 
+      <Users />         
+      </div>
+
+     
       </div>
     );
   }
