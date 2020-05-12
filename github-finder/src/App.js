2d139e1e66a82cad7c45503f81445c517ccab8c3
darms <darmcca@gmail.com>
2020-05-12 16:49:14 -0700

    added axios

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   componentDidMount() {
-    console.log(1234);
+    axios.get('https://api.github.com/users')
 
   }
 
